0b315a0a711507f0839c3bad3a99361caba727eb
zjh <1017328759@qq.com>
2022-05-20 10:34:50 +0800

    高响应比HRRN

--- src/com/zjh/work03HRRN.java
+++ src/com/zjh/work03HRRN.java
@@ -1,28 +1,26 @@
 package com.zjh;
 
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * @author 张俊鸿
- * @description: 时间片轮转算法
- * @since 2022-05-09 19:20
+ * @description: 高响应比调度算法
+ * @since 2022-05-20 9:14
  */
-public class work01RR {
+public class work03HRRN {
     static Scanner scanner = new Scanner(System.in);
-
     /**
      * 进程控制块PCB
      */
     static class PCB implements Comparable<PCB>{
         String name; // 进程名
         int arriveTime; // 到达时间
-        int serveTime; // 运行时间
-        int hasRunTime = 0; // 已经运行时间
+        int serveTime; // 期待运行时间
+        int waitTime; // 等待时间
         int beginTime; // 开始时间
-        int turnRoundTime; //周转时间
+        int hasRunTime = 0; // 已经运行时间
+        int turnRoundTime; // 周转时间
+        double priority; // 优先权 【还没到达优先权为0】
         @Override
         public int compareTo(PCB o) { // 按照 到达时间 进入就绪队列
             return  this.arriveTime - o.arriveTime;
@@ -35,7 +33,6 @@ public class work01RR {
         }
     }
     static PCB[] pcbArrays; // 进程数组
-    static int timeSlice; //时间片
     //使用优先队列将输入的进程自动按照到达时间排序,模拟按照时间到达
     static Queue<PCB> pcb_queue = new PriorityQueue<>();
 
@@ -46,8 +43,6 @@ public class work01RR {
         System.out.print("请输入进程数：");
         int pcb_num = scanner.nextInt();
         pcbArrays = new PCB[pcb_num];
-        System.out.print("请输入时间片大小：");
-        timeSlice = scanner.nextInt();
         for (int i = 0; i < pcb_num; i++) {
             System.out.println("进程号"+ (i+1));
             System.out.print("请输入进程名：");
@@ -65,12 +60,12 @@ public class work01RR {
     }
 
     /**
-     * 检查是否有到达的进程，有就加入就绪队列中
+     * 检查是否有到达的进程，有就加入就绪列表中
      *
-     * @param readyQueue 就绪队列
+     * @param readyList 就绪列表
      * @param cpuTime    cpu当前的时间
      */
-    public static void check(Queue<PCB> readyQueue, int cpuTime){
+    public static void check(List<PCB> readyList, int cpuTime){
         while (true){
             //查看到达的进程，直到有进程没到达就退出
             PCB pcb_first = pcb_queue.peek();
@@ -79,38 +74,45 @@ public class work01RR {
                 break;
             }else if(pcb_first.arriveTime <= cpuTime){//判断是否到达
                 //已就绪，弹出排队队列，加入就绪队列
-                readyQueue.offer(pcb_queue.poll());
+                readyList.add(pcb_queue.poll());
             }else {
                 //没有就绪的进程
                 break;
             }
         }
     }
-
-    public static void printCurrent(PCB pcb,int cpuTime){
-        System.out.println("当前时间:"+cpuTime+"\t运行的进程:"+pcb.name+"\t剩余运行时间:"+(pcb.serveTime-pcb.hasRunTime));
+    /**
+     * 计算优先级并弹出最大优先级的进程
+     *
+     * @param readyList 就绪列表
+     * @param cpuTime   cpu时间
+     * @return {@link PCB}
+     */
+    public static PCB getByPriority(List<PCB> readyList, int cpuTime){
+        //遍历list
+        double max = 0.0;
+        int index = 0;
+        for (int i = 0; i < readyList.size(); i++) {
+            readyList.get(i).waitTime = cpuTime - readyList.get(i).arriveTime;
+            readyList.get(i).priority = (readyList.get(i).waitTime + readyList.get(i).serveTime) / readyList.get(i).serveTime;
+            if(readyList.get(i).priority > max) {
+                max = readyList.get(i).priority;
+                index = i;
+            }
+        }
+        PCB pcb = readyList.get(index);
+        readyList.remove(index);
+        return pcb;
     }
 
     /**
-     * 一个时间片内进程运行
+     * 打印当前进程
      *
-     * @param pcb     进程控制块
+     * @param pcb     印刷电路板
      * @param cpuTime cpu时间
-     * @return boolean 是否提前退出
      */
-    static boolean pcbRun(PCB pcb,int cpuTime){
-        if(pcb.hasRunTime == 0){
-            //首次响应时间
-            pcb.beginTime = cpuTime;
-        }
-        printCurrent(pcb,cpuTime);
-        pcb.hasRunTime++;
-        if(pcb.hasRunTime == pcb.serveTime){
-            //进程结束
-            pcb.turnRoundTime = cpuTime + 1 - pcb.arriveTime;
-            return true;
-        }
-        return false;
+    public static void printCurrent(PCB pcb, int cpuTime){
+        System.out.println("当前时间:"+cpuTime+"\t运行的进程:"+pcb.name+"\t剩余运行时间:"+(pcb.serveTime-pcb.hasRunTime));
     }
 
     /**
@@ -122,44 +124,50 @@ public class work01RR {
         if(!pcb_queue.isEmpty()){
             cpuTime = pcb_queue.peek().arriveTime;
         }
-        Queue<PCB> readyQueue = new LinkedList<>(); //就绪队列
-        int timeSliceFlag = timeSlice;
+        List<PCB> readyList = new ArrayList<>();
+        //当前运行的进程
         PCB runningPcb = null;
         while (true){
             System.out.println();
             //判断是否结束
-            if(pcb_queue.isEmpty() && readyQueue.isEmpty() && runningPcb== null){
+            if(pcb_queue.isEmpty() && readyList.isEmpty() && runningPcb == null){
                 System.out.println("======所有进程运行结束======");
                 break;
             }else {
                 //处理进程
-                if(timeSliceFlag % timeSlice == 0){
-                    //一个时间片结束
-                    check(readyQueue,cpuTime);
-                    if(runningPcb != null){
-                        readyQueue.offer(runningPcb);
+                //先查看是否有到达的进程
+                check(readyList,cpuTime);
+                //上一个运行完毕，要取出新的
+                if(runningPcb == null){
+                    runningPcb = getByPriority(readyList,cpuTime);
+                    //响应时间
+                    runningPcb.beginTime = cpuTime;
+                    printCurrent(runningPcb,cpuTime);
+                    runningPcb.hasRunTime++;
+                    cpuTime++;
+                }else {
+                    //当前有进程正在运行
+                    //判断是否结束
+                    if(runningPcb.hasRunTime == runningPcb.serveTime){
+                        //进程结束
+                        runningPcb.turnRoundTime = cpuTime - runningPcb.arriveTime;
+                        System.out.println("======进程"+runningPcb.name+"运行结束======");
+                        //置空
                         runningPcb = null;
-                    }
-                    //取出要运行的进程
-                    runningPcb = readyQueue.poll();
-                    if(runningPcb == null){
-                        //当前没有可以运行的pcb
-                        cpuTime++;
                         continue;
                     }
+                    printCurrent(runningPcb,cpuTime);
+                    runningPcb.hasRunTime++;
+                    cpuTime++;
                 }
-                //时间片没到，判断是否有进程提前结束
-                if(pcbRun(runningPcb, cpuTime)){
-                    runningPcb = null;
-                    timeSliceFlag = timeSlice;
-                }else {
-                    timeSliceFlag++;
-                }
-                cpuTime++;
             }
         }
     }
-    // 周转时间
+
+
+    /**
+     * 显示周转时间
+     */
     private static void showTurnAroundTime() {
         double averageT = 0;
         double averageWTAT = 0;
@@ -179,9 +187,17 @@ public class work01RR {
         System.out.println("平均周转时间：" + averageT);
         System.out.printf("平均带权周转时间：%.2f\n", averageWTAT);
     }
+
     public static void main(String[] args) {
         input();
         run();
         showTurnAroundTime();
     }
+
+
+
+
+
+
+
 }
